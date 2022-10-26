adac352489c1a3998e00f5b5bfdeef6415616ae4
Denis-92 <d.mozenic@gmail.com>
2022-10-26 16:29:36 +0200

    class Movie via require

--- class/Movie.php
+++ class/Movie.php
@@ -1,35 +1,40 @@
 <?php
 
+// CLASSE
 class Movie {
 
-// VARIABILI D'ISTANZA
-public $title;
-public $length = "";
-public $language = "";
-public $genre = "";
-public $type = "";
-public $actors = [];
+    // VARIABILI D'ISTANZA
+    public $title;
+    public $length = "";
+    public $language;
+    public $genre;
+    public $type;
 
-// COSTRUTTORE
-function __construct( $_genre ) {
-    $this->genre = $_genre;
-}
-
-// METODO
-public function setType ( $length ) {
-    if ( $length < 60 ) {
-        $this->type = "Cortometraggio";
-    } else {
-        $this->type = "Cinema Movie";
+    // COSTRUTTORE
+    function __construct( $_genre , $_language = "" ) {
+        $this->genre = $_genre;
+        $this->language = $_language;
     }
+
+    // METODO
+    public function setTypeLength ( $length ) {
+        if ( $length < 60 ) {
+            $this->type = "Cortometraggio";
+        } else {
+            $this->type = "Cinema Movie";
+        }
+        $this->length = $length;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function getLength() {
+        return $this->length;
+    }
+
+
 }
-
-public function getType() {
-    return $this->type;
-}
-
-
-}
-
 
 ?>
--- index.php
+++ index.php
@@ -1,46 +1,11 @@
 <?php
 
-// CLASSE
-class Movie {
+require_once __DIR__ . "/class/Movie.php";
 
-    // VARIABILI D'ISTANZA
-    public $title;
-    public $length = "";
-    public $language;
-    public $genre;
-    public $type;
-
-    // COSTRUTTORE
-    function __construct( $_genre , $_language = "" ) {
-        $this->genre = $_genre;
-        $this->language = $_language;
-    }
-
-    // METODO
-    public function setTypeLength ( $length ) {
-        if ( $length < 60 ) {
-            $this->type = "Cortometraggio";
-        } else {
-            $this->type = "Cinema Movie";
-        }
-        $this->length = $length;
-    }
-
-    public function getType() {
-        return $this->type;
-    }
-
-    public function getLength() {
-        return $this->length;
-    }
-
-
-}
-
-$sala1 = new Movie("Avventura");
+$sala1 = new Movie("Azione");
 echo "<p>Genere: " . $sala1->genre . "</p>";
 
-$sala1->setTypeLength(65);
+$sala1->setTypeLength(55);
 
 $lengthMovie_sala1 = $sala1->getLength();
 echo "<p>Durata: " . $lengthMovie_sala1 . " minuti</p>";
