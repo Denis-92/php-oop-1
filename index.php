8780ad3e7a1263fb83349fe320f9bbe5a901f603
Denis-92 <d.mozenic@gmail.com>
2022-10-26 16:30:57 +0200

    add comments

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 require_once __DIR__ . "/class/Movie.php";
 
+// SETTARE GENERE MOVIE
 $sala1 = new Movie("Azione");
 echo "<p>Genere: " . $sala1->genre . "</p>";
 
+// SETTARE DURATA MOVIE IN MINUTI
 $sala1->setTypeLength(55);
 
 $lengthMovie_sala1 = $sala1->getLength();
