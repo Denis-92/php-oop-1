db5d4cb1a9983b9252b940d3060bf76dd8dd0cde
Denis-92 <d.mozenic@gmail.com>
2022-10-26 13:58:56 +0200

    add example construct $_eta

--- index.php
+++ index.php
@@ -2,32 +2,39 @@
 
 class user {
     public $nome;
-    public $email;
+    public $eta;
 
-    public $sconto = 0;
-
-    public function setSconto($eta){
-        if($eta>65) {
-            $this->sconto = 40;
-        }
+    function __construct($_eta) {
+        $this->eta = $_eta;
     }
 
-    public function getSconto() {
-        return $this->sconto;
-    }
+
+    // public $email;
+
+    // public $sconto = 0;
+
+    // public function setSconto($eta){
+    //     if($eta>65) {
+    //         $this->sconto = 40;
+    //     }
+    // }
+
+    // public function getSconto() {
+    //     return $this->sconto;
+    // }
 
 }
 
-$pippo = new user();
+// $pippo = new user();
 
-$pippo->setSconto(40);
-$sconto_pippo = $pippo->getSconto();
+// $pippo->setSconto(40);
+// $sconto_pippo = $pippo->getSconto();
 
 // $pippo->nome = "Pippo";
 // $pippo->email = "pippo@email.it";
 
 
-// $paperino = new user();
-
+$paperino = new user(50);
+echo $paperino->eta;
 
 ?>
