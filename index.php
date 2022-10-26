d52e978127f52baf5e4381a3024557fe36f3a6c5
Denis-92 <d.mozenic@gmail.com>
2022-10-26 13:53:03 +0200

    add public in class and content variable

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 
-class user {}
+class user {
+    public $nome;
+    public $email;
+}
 
 $pippo = new user();
+
+$pippo->nome = "Pippo";
+$pippo->email = "pippo@email.it";
+
+
 $paperino = new user();
 
 
