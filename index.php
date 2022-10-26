5efd665b14e0a41d8fbc391a0854bba735b95e8e
Denis-92 <d.mozenic@gmail.com>
2022-10-26 13:55:34 +0200

    add set/get sconto

--- index.php
+++ index.php
@@ -3,15 +3,31 @@
 class user {
     public $nome;
     public $email;
+
+    public $sconto = 0;
+
+    public function setSconto($eta){
+        if($eta>65) {
+            $this->sconto = 40;
+        }
+    }
+
+    public function getSconto() {
+        return $this->sconto;
+    }
+
 }
 
 $pippo = new user();
 
-$pippo->nome = "Pippo";
-$pippo->email = "pippo@email.it";
+$pippo->setSconto(40);
+$sconto_pippo = $pippo->getSconto();
+
+// $pippo->nome = "Pippo";
+// $pippo->email = "pippo@email.it";
 
 
-$paperino = new user();
+// $paperino = new user();
 
 
 ?>
